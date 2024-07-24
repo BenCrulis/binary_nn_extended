8efe0f4724d360c1dc68c7a413ecb60e178663de
Ben Crulis <>
2024-07-24 10:24:38 +0200

    implemented SPSA-H

---
+++ binary_nn/algorithms/spsa_h.py
@@ -0,0 +1,91 @@
+import gc
+from copy import deepcopy
+
+import torch
+from torch import nn
+from torch.optim import Optimizer
+
+from torchmetrics.functional import accuracy
+
+from binary_nn.models.common.binary.modules import Sign, SignUnsat
+from utils.configuration import ConfigurableMixin
+
+
+class ExpendableTensor(torch.Tensor):
+    def __add__(self, other):
+        if self.shape[0] == other.shape[0] * 2:
+            return torch.add(self, other.repeat((2, *[1 for i in other.shape[1:]])))
+        return torch.add(self, other)
+
+
+class SPSAH(ConfigurableMixin):
+    def __init__(self, c=1e-3, distribution="gaussian", modules_to_hook=(nn.Linear, nn.Conv1d, nn.Conv2d)):
+        super().__init__()
+        self.c = c
+        self.modules_to_hook = modules_to_hook
+        self.distribution = distribution
+
+    def config(self):
+        return {"SPSA-h distribution": self.distribution}
+
+    def _generate_perturbation(self, shape, device=None):
+        if self.distribution == "rademacher":
+            return torch.distributions.Bernoulli(probs=torch.tensor([0.5], device=device)).sample(shape).squeeze(-1) * 2 - 1
+        elif self.distribution == "gaussian":
+            return torch.randn(shape, device=device)
+
+    @torch.no_grad()
+    def __call__(self, model: nn.Module, x, y, opt: Optimizer, loss_fn):
+
+        # compute statistics for batchnorm and other similar modules
+        model.train()
+        y_pred = model(x)
+        l = loss_fn(y_pred, y)
+
+        model.eval()
+
+        candidate_modules = []
+
+        for module in model.modules():
+            if isinstance(module, self.modules_to_hook):
+                candidate_modules.append(module)
+
+        i = torch.randint(0, len(candidate_modules), (1,)).item()
+        mod = candidate_modules[i]
+
+        perturbation = [None]
+        saved_out = [None]
+        handle = [None]
+
+        def forward_hook(mod, input, output):
+            handle[0].remove()
+            input = input[0] if isinstance(input, tuple) else input
+            input = input.detach()
+            with torch.enable_grad():
+                out = mod(input)
+                saved_out[0] = out
+
+            pert = self._generate_perturbation(out.shape)
+            perturbation[0] = pert
+            out_pos = out + pert*self.c
+            out_neg = out - pert*self.c
+            new_out = torch.cat([out_pos, out_neg], dim=0)
+            return ExpendableTensor(new_out)
+
+        handle[0] = mod.register_forward_hook(forward_hook)
+        y_pred_pert = model(x)
+
+        l_plus = loss_fn(y_pred_pert[:len(x)], y)
+        l_minus = loss_fn(y_pred_pert[len(x):], y)
+
+        # compute the final gradient
+        gain = (l_plus - l_minus) / (2.0 * self.c)
+
+        opt.zero_grad()
+        saved_out[0].backward(perturbation[0]*gain)
+        opt.step()
+
+        return l, y_pred
+
+
+
